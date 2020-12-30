e2a5c95763fe8269ee08f684d4881909db998a3e
Craig Martin <c.martin0297@gmail.com>
2020-12-30 21:52:04 +0000

    Create basic navigation sidebar that is collapsable with toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,13 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
 import { AboutComponent } from './components/about/about.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
-import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list'
+import { MatListModule } from '@angular/material/list';
+import { MainNavComponent } from './components/main-nav/main-nav.component'
 
 @NgModule({
   declarations: [
